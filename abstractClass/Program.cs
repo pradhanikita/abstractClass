b441f718759886e282d4e99d0b65d5bdb8050d55
dell <dell@DESKTOP-L36T5DT>
2022-03-08 11:09:53 +0545

    Add project files.

---
+++ abstractClass/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace abstractClass
+{
+    abstract class Person
+    {
+        public string Name;
+        public void name()
+        {
+            Console.WriteLine("Nikita");
+        }
+    }
+
+    interface Department
+    {
+        void calculateArea(int length, int breadth);
+        
+    }
+    public class Person2 : Department
+    {
+        public void calculateArea(int l, int b)
+        {
+        int area = l * b;
+        Console.WriteLine(area);
+           
+        }
+    }
+    internal class Program:Person
+    {
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.Name = "Rojan";
+            obj.name();
+            Console.WriteLine("My name is "+ obj.Name);
+
+            Person2 p = new Person2();
+            p.calculateArea(20, 20);
+     
+            Console.ReadLine();
+        }
+    }
+}
